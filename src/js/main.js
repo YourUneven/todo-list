98de20eb02a53d0a91af33eb4a057b4529ed198f
YourUneven <150155156+YourUneven@users.noreply.github.com>
2024-09-30 10:50:05 +0300

    start js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
 import '../scss/style.scss'
+const todoValue = document.getElementById('#todoText'),
+ listItems = document.getElementById('#list-items'),
+ AddUpdateClick = document.getElementById('#AddUpdateClick');
+
+
+ function CreateTodoData() {
+  alert(todoValue.value);
+  if (todoValue.value) {
+
+  }
+ }
