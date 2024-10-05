a7c2ba20979f3ad3ef8818d0dc06224f02b4720d
YourUneven <150155156+YourUneven@users.noreply.github.com>
2024-10-05 13:35:48 +0300

    05.10.24

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,19 @@ import '../scss/style.scss'
 
  const popupText = document.querySelector('#popupText'),
 popupButtonApply = document.querySelector('#popupApply'),
- listItems = document.getElementById('lisning');
+ listItems = document.getElementById('lisning'),
+ buttonDefault = document.querySelector('.img-default');
 
- 
+
 popupButtonApply.addEventListener('click', addItemsBlock);
 
 let todolist = [];
 
+if(localStorage.getItem('todo')) {
+  todolist = JSON.parse(localStorage.getItem('todo'));
+  displayMessages();
+}
 function addItemsBlock() {
-  
   let newTodo = {
     todo: popupText.value,
     checked: false,
@@ -18,36 +22,42 @@ function addItemsBlock() {
   };
 todolist.push(newTodo);
 displayMessages();
+localStorage.setItem('todo', JSON.stringify(todolist));
 }
+
 
 
 function displayMessages() {
   let displayMessage = '';
   todolist.forEach(function(item, i) {
  displayMessage += `
- <li>
-<div class="list-items__sub">
-<input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}/>
- <label for="${i}"><span>${item.todo}</span></label>
-    </div>
-    <div class="img-search">
-    <a href="#"><img class="img-redact" src="/public/images/Vector (3).png" alt="#"></a>
-    <a href="#"><img class="img-delat" src="/public/images/trash-svgrepo-com 1.png" alt=""></a></li>`;
+<li>
+<input type="checkbox"id='item_${i}' ${item.checked ? 'checked' : ''}>
+  <label for="item_${i}">${item.todo}</label>
+
+
+<div class="img-search">
+  
+  <a class="img-redact" href="#">
+<img  src="/public/images/Vector (3).png"onClick="editTask(${item}) alt="#"></a>
+<a class="img-defauit" href="#"><img " src="/public/images/trash-svgrepo-com 1.png"onClick="deleteTask(${item}) alt=""></a> 
+</li>`;
     listItems.innerHTML = displayMessage;
 
   });
 }
-//   const item = document.createElement('li')
-// item.innerHTML = ``;
-//   const listItems = document.getElementById('lisning');
-//   lisning.appendChild(item);
 
-listItems.addEventListener('change', function(event) {
- let idInput = console.log(event.target.getAttribute('id'));
- let forLabel = listItems.querySelector('[for=" + idInput +  "]');
-  console.log('forLabel', forLabel);
-  
+ listItems.addEventListener('change', function(event) {
+ let valueLabel = listItems.querySelector('[for=' + event.target.getAttribute('id') +' ]').innerHTML;
+
+
+  todolist.forEach(function(item) {
+    if (item.todo === valueLabel) {
+      item.checked = !item.checked;
+      localStorage.setItem('todo', JSON.stringify(todolist));
+    }
+  });
 });
 
- 
- 
+
+
